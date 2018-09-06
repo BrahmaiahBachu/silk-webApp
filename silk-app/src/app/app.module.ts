2682a30dbfb84dde7206389848dad380b250d598
Shadab Mehdi <ausshadu@gmail.com>
2018-09-06 14:13:33 +0530

    Updated all libraries and added PrimeNG UI Components

--- silk-app/src/app/app.module.ts
+++ silk-app/src/app/app.module.ts
@@ -1,10 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from './section/section.component';
+
+// Custom Modules Importing Below
+import { ButtonModule } from 'primeng/button';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +21,9 @@ import { SectionComponent } from './section/section.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
